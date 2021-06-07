6bc8da00c514dcdeb6eca00e78fac4fd522719a4
Adina <adinairina7@gmail.com>
2021-06-07 19:30:36 +0300

    add to cart

--- angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
 import {ProductService} from "../../services/product.service";
+import {ActivatedRoute} from "@angular/router";
+import {CartService} from "../../services/cart.service";
+import {Product} from "../../common/product";
+import {CartItem} from "../../common/cart-item";
 
 @Component({
   selector: 'app-product-details',
@@ -8,21 +11,28 @@ import {ProductService} from "../../services/product.service";
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
+
   public product;
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute,private cartService: CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => this.productDetailPage());
   }
-  productDetailPage(){
+
+  productDetailPage() {
     const hasProductId = this.route.snapshot.paramMap.has('id');
-    if(hasProductId){
-      this.productService.listProductDetailPage(+this.route.snapshot.paramMap.get('id')).subscribe(data => {
+    if(hasProductId) {
+      this.productService.listProductDetailPage(+this.route.snapshot.paramMap.get('id')).subscribe(
+        data => {
           console.log(data);
           this.product = data;
         }
       );
     }
+  }
+  addToCart(product: Product) {
+    const cartItem = new CartItem(product);
+    this.cartService.addToCart(cartItem);
   }
 }
